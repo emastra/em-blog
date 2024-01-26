cbc77f59a0a802afedf0f60cd5ef71d1eb68ec14
emastra <emiliano.mastra@gmail.com>
2024-01-26 11:44:21 +0100

    create MainContainer

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -4,10 +4,6 @@ interface Props {
   children: ReactNode
 }
 
-// !
-// Used only in LayoutWrapper, PostBanner, PostLayout, PostSimple components. Which are not used.
-// For main use, I created MainContainer component.
-
 export default function SectionContainer({ children }: Props) {
   return (
     <section className="mx-auto max-w-5xl px-4 sm:px-6 xl:max-w-[1080px] xl:px-2">
