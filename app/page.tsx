1298c85c70cdcc2aa2857efa08eef78bf51898c7
emastra <emiliano.mastra@gmail.com>
2024-01-30 11:03:58 +0100

    new pages and home;

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
+// import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
+// import { allBlogs } from 'contentlayer/generated'
+// import Main from './Main'
 
-export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+// export default async function Page() {
+//   const sortedPosts = sortPosts(allBlogs)
+//   const posts = allCoreContent(sortedPosts)
+
+//   return <Main posts={posts} />
+// }
+
+import HomeListLayout from '@/layouts/HomeListLayout'
+import Hero from '@/components/Hero'
+import MainContainer from '@/components/MainContainer'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import { allBlogs } from 'contentlayer/generated'
+import { genPageMetadata } from 'app/seo'
+
+const POSTS_PER_PAGE = 10
+
+export const metadata = genPageMetadata({ title: 'Blog' })
+
+export default function BlogPage() {
+  const posts = allCoreContent(sortPosts(allBlogs))
+  const pageNumber = 1
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
+
+  return (
+    <>
+      <Hero />
+      <MainContainer>
+        <HomeListLayout
+          posts={posts}
+          initialDisplayPosts={initialDisplayPosts}
+          pagination={pagination}
+          title="All Posts"
+        />
+      </MainContainer>
+    </>
+  )
 }
