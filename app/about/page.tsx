9c306669d035f47f456af81cfe8e94221d871706
emastra <emiliano.mastra@gmail.com>
2024-01-30 15:24:29 +0100

    About page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
+import MainContainer from '@/components/MainContainer'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
@@ -12,9 +13,13 @@ export default function Page() {
 
   return (
     <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
+      <MainContainer>
+        <div className="mt-16">
+          <AuthorLayout content={mainContent}>
+            <MDXLayoutRenderer code={author.body.code} />
+          </AuthorLayout>
+        </div>
+      </MainContainer>
     </>
   )
 }
