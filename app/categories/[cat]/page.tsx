b6b2e058d6abc28fc4103c02bcfcedaea72d7a31
emastra <emiliano.mastra@gmail.com>
2024-01-30 15:19:00 +0100

    sticky tag list box

--- app/categories/[cat]/page.tsx
+++ app/categories/[cat]/page.tsx
@@ -38,6 +38,7 @@ export default function TagPage({ params }: { params: { cat: string } }) {
   const filteredPosts = allCoreContent(
     sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
   )
+
   return (
     <MainContainer>
       <ListLayout posts={filteredPosts} title={title} />
