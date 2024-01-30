ef7fde44dd0b1889236319f9960cff8428552aa9
emastra <emiliano.mastra@gmail.com>
2024-01-30 14:01:46 +0100

    articles next page

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -18,6 +18,7 @@ interface ListLayoutProps {
   title: string
   initialDisplayPosts?: CoreContent<Blog>[]
   pagination?: PaginationProps
+  hasSearch?: boolean
 }
 
 function Pagination({ totalPages, currentPage }: PaginationProps) {
@@ -65,6 +66,7 @@ export default function ListLayout({
   title,
   initialDisplayPosts = [],
   pagination,
+  hasSearch = true,
 }: ListLayoutProps) {
   const [searchValue, setSearchValue] = useState('')
   const filteredBlogPosts = posts.filter((post) => {
@@ -84,32 +86,34 @@ export default function ListLayout({
             {title}
           </h1> */}
           {/* IF WANT INPUT IN THE CENTER: mx-auto  */}
-          <div className="relative max-w-lg">
-            <label>
-              <span className="sr-only">Cerca tra tutti gli articoli</span>
-              <input
-                aria-label="Search articles"
-                type="text"
-                onChange={(e) => setSearchValue(e.target.value)}
-                placeholder="Cerca tra tutti gli articoli"
-                className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
-              />
-            </label>
-            <svg
-              className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </div>
+          {hasSearch && (
+            <div className="relative max-w-lg">
+              <label>
+                <span className="sr-only">Cerca tra tutti gli articoli</span>
+                <input
+                  aria-label="Search articles"
+                  type="text"
+                  onChange={(e) => setSearchValue(e.target.value)}
+                  placeholder="Cerca tra tutti gli articoli"
+                  className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+                />
+              </label>
+              <svg
+                className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
+          )}
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!filteredBlogPosts.length && 'No posts found.'}
