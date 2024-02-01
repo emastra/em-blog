b1ce5398f1248f92019ad944f6aba81e515a0033
emastra <emiliano.mastra@gmail.com>
2024-02-01 21:29:05 +0100

    articles page title

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -81,10 +81,10 @@ export default function ListLayout({
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="mb-8 space-y-2 pb-8 pt-6 md:space-y-5">
-          {/* <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <div className="mb-4 space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="mb-16 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
-          </h1> */}
+          </h1>
           {/* IF WANT INPUT IN THE CENTER: mx-auto  */}
           {hasSearch && (
             <div className="relative max-w-lg">
