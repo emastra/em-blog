34b50dd078be822ea734c833a8c4c561c64237df
emastra <emiliano.mastra@gmail.com>
2024-02-01 22:05:47 +0100

    dates in articles page

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -120,40 +120,15 @@ export default function ListLayout({
           {displayPosts.map((post) => {
             const { path, date, title, summary, tags } = post
             return (
-              // <li key={path} className="py-4">
-              //   <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-              //     <dl>
-              //       <dt className="sr-only">Published on</dt>
-              //       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-              //         <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-              //       </dd>
-              //     </dl>
-              //     <div className="space-y-3 xl:col-span-3">
-              //       <div>
-              //         <h3 className="text-2xl font-bold leading-8 tracking-tight">
-              //           <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
-              //             {title}
-              //           </Link>
-              //         </h3>
-              //         <div className="flex flex-wrap">
-              //           {tags?.map((tag) => <Tag key={tag} text={tag} />)}
-              //         </div>
-              //       </div>
-              //       <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-              //         {summary}
-              //       </div>
-              //     </div>
-              //   </article>
-              // </li>
               <li key={path} className="py-12">
                 <article>
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    {/* <dl>
+                    <dl>
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
                       </dd>
-                    </dl> */}
+                    </dl>
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
