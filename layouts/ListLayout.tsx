8d833a4d26e1c056b1534db7030a739b0aa6423f
emastra <emiliano.mastra@gmail.com>
2024-02-02 21:47:54 +0100

    fix spacing and font size

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -81,8 +81,8 @@ export default function ListLayout({
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="mb-4 space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="mb-8 text-5xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:text-6xl md:leading-14">
+        <div className="mb-8 space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-5xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
           {/* IF WANT INPUT IN THE CENTER: mx-auto  */}
