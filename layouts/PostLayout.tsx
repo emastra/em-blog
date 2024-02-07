9ee1ef76e3917be61a9835713581c4a6a545f5f3
emastra <emiliano.mastra@gmail.com>
2024-02-07 10:05:52 +0100

    minor margin

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -47,35 +47,14 @@ export default function PostLayout({
       <ArticleHeader title={title} />
       <MainContainer>
         <ScrollTopAndComment />
-
         <article>
           <div className="">
-            {/* header with title */}
-            {/* <header className="mt-8 pt-6 xl:pb-6">
-              <div className="space-y-1">
-                <dl className="space-y-10">
-                  <div>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>
-                        {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                      </time>
-                    </dd>
-                  </div>
-                </dl>
-                <div>
-                  <PageTitle>{title}</PageTitle>
-                </div>
-              </div>
-            </header> */}
-
             <div className="grid grid-cols-6 gap-8">
-              <div className="col-span-6 md:order-last md:col-span-2 md:ml-12 lg:ml-16">
+              <div className="col-span-6 mt-1 md:order-last md:col-span-2 md:ml-12 lg:ml-16">
                 <div className="sticky top-[164px]">
                   <TableOfContents toc={toc} />
                 </div>
               </div>
-
               <div className="col-span-6 md:col-span-4 md:mr-4">
                 <div className="prose max-w-none pb-8 dark:prose-invert">{children}</div>
               </div>
