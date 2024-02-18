974a8f5e57c21a5d89d5075f6223f14b2efaebf0
emastra <emiliano.mastra@gmail.com>
2024-02-18 10:59:00 +0100

    add categoriesData

---
+++ data/categoriesData.ts
@@ -0,0 +1,17 @@
+interface Category {
+  href: string
+  name: string
+  description?: string
+  imgSrc?: string
+}
+
+const categoriesData: Category[] = [
+  { href: '', name: 'Orientamento', description: '' },
+  { href: '', name: 'Html', description: '' },
+  { href: '', name: 'Css', description: '' },
+  { href: '', name: 'Javascript', description: '' },
+  { href: '', name: 'Linux', description: '' },
+  { href: '', name: 'Web', description: '' },
+]
+
+export default categoriesData
