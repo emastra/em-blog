ba5660b87f43608dc16eff6a7d7e64227f7f09d2
emastra <emiliano.mastra@gmail.com>
2024-02-22 15:55:10 +0100

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,11 +10,16 @@ export default function sitemap(): MetadataRoute.Sitemap {
     .map((post) => ({
       url: `${siteUrl}/${post.path}`,
       lastModified: post.lastmod || post.date,
+      // Additional optional properties (presi da MetadataRoute.Sitemap):
+      // changeFrequency: 'weekly', // Assuming the blog is updated weekly
+      // priority: 0.8, // Assigning a relatively high priority
     }))
 
-  // TODO: do something here
-  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
+  // IT WAS: const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
+  // lascio fuori 'blog' al momento. TODO: se aggiungo il redirect di /blog dovrei aggiungere la route pure qui
+  const routes = ['', 'articles', 'categories', 'about'].map((route) => ({
     url: `${siteUrl}/${route}`,
+    // TODO: dovrei mettere una data piu realistica. Articles cambia ogni volata che aggiungo un articolo. About non cambia quasi mai
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
