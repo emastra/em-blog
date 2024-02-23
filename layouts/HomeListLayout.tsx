f29d703733a8e13af9e79451bc1620201aa97aa5
emastra <emiliano.mastra@gmail.com>
2024-02-23 12:59:29 +0100

    improved article html

--- layouts/HomeListLayout.tsx
+++ layouts/HomeListLayout.tsx
@@ -57,31 +57,23 @@ export default function HomeListLayout({
                     <div className="mb-4 ml-[-4px]">
                       <CategoryLabel name={category} />
                     </div>
-
-                    <div className="group">
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
-                          {title}
-                        </Link>
+                    <Link href={`/${path}`} className="group">
+                      <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                        {title}
                       </h3>
-                      <div className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <div>Lorem ipsum lorem ipsum ya ya ya lorem ipsum</div>
-                      </div>
-
-                      <div className="prose mt-3 max-w-none text-gray-800 dark:text-gray-200">
+                      <h4 className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        Lorem ipsum lorem ipsum ya ya ya lorem ipsum subtitle
+                      </h4>
+                      <p className="prose mt-3 max-w-none text-gray-800 dark:text-gray-200">
                         {summary}
+                      </p>
+                      <div
+                        aria-label={`Read more: "${title}"`}
+                        className="mt-3 text-base font-medium leading-6 text-gray-900 group-hover:text-primary-600 dark:text-gray-100"
+                      >
+                        Read more &rarr;
                       </div>
-
-                      <div className="mt-3 text-base font-medium leading-6">
-                        <Link
-                          href={`/${path}`}
-                          className="text-gray-900 group-hover:text-primary-600 dark:text-gray-100"
-                          aria-label={`Read more: "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                    </div>
+                    </Link>
                   </article>
                 </li>
               )
